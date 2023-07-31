d054a69da845c0ab5c1e0e6e9052fb1a595f7345
Austin Smith <personal@austintrentsmith.com>
2023-07-30 20:16:04 -0500

    Prep for Digga branch

--- pkgs/berkeley-mono-font/default.nix
+++ pkgs/berkeley-mono-font/default.nix
@@ -1,19 +1,19 @@
-{ lib, stdenv, fetchGitHub, ... }:
+{ lib, stdenv, ... }:
 
 stdenv.mkDerivation rec {
   pname = "berkeley-mono-font";
   version = "1.0.0";
   
-   src = fetchFromGitHub {
-      owner = "Asmith9555";
-      repo = "berkeley-mono-font";
+   src = builtins.fetchGit {
+      url = "git@github.com:Asmith9555/berkeley-mono-font.git";
       rev = "9d214f4ecdac02bb5c4ac5925a474622d93e89cf";
-      sha256 = "03c4b734d8be254fe81b1834eee799f75a93c51e75e837b3a2fb3338144968d2";
+      ref = "ttf";
+      # sha256 = "03c4b734d8be254fe81b1834eee799f75a93c51e75e837b3a2fb3338144968d2";
    };
 
   installPhase = ''
-    mkdir -p $out/share/fonts/truetype/BerkeleyMono/
-    mv $src/TTF/* $out/share/fonts/truetype/BerkeleyMono/
+    mkdir -p $out/share/fonts/truetype/BerkeleyMono
+    cp $src/*.ttf $out/share/fonts/truetype/BerkeleyMono
   '';
 
   meta = {
